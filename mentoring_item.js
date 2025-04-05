a57ee34c436c70e9d119eac0e76c20f865a16f2b
Snow <skymygo6809@gmail.com>
2025-04-05 23:26:29 +0900

    [FEAT] 1.멘토링 목록 페이지에서 기존에 신청한 멘토링과 시간이 겹치면 빨간 글씨로 표시 2. 멘토링 상세 페이지에서 기존에 신청한 멘토링 시간과 겹치면 alert 창으로 경고 표시시

---
+++ mentoring_item.js
@@ -0,0 +1,29 @@
+getAllMentoringEvents().then((lectures) => {
+    console.log(lectures)
+    const lecturesDictionary = convertLectureDictionaryWithoutDate(lectures);
+    
+    console.log(lecturesDictionary)
+    
+
+    const timeStr = document.querySelector(" div.top > div:nth-child(3) > div:nth-child(2) > div.c").innerText;
+    let [datePart, timePart] = timeStr.split(/\s{2,}/); // 공백 2개 이상 기준으로 나눔
+    const [startTime, endTime] = timePart.replace(/시/g, '').split(' ~ ');
+
+    datePart = datePart.replaceAll(".", "-")
+
+    if(!lecturesDictionary.hasOwnProperty(datePart)){
+        return
+    }
+    const targetList = lecturesDictionary[datePart]
+    for (let j = 0; j < targetList.length; j++) {
+        const [targetStartTime, targetEndTime ] = targetList[j].split(" ~ ")
+        if (getMin(endTime) < getMin(targetStartTime))
+            break
+
+        if (getMin(startTime) > getMin(targetEndTime))
+            continue
+
+        alert("시간이 겹치는 강의입니다. 신청하시기 전에 주의해주세요.")
+
+    }
+});
---
+++ utils.js
@@ -0,0 +1,113 @@
+function normalizeTimeStr(time) {
+    const [h, m, s] = time.split(':');
+    return `${h.padStart(2, '0')}:${m.padStart(2, '0')}:${s.padStart(2, '0')}`;
+  }
+  
+function extractEventListFromHTML(html) {
+  const container = document.createElement('div');
+  container.innerHTML = html;
+
+  const rows = container.querySelectorAll("#contentsList > div > div > div.boardlist > div.tbl-ovx > table > tbody > tr");
+  const events = [];
+
+  for (const row of rows) {
+    const tds = row.querySelectorAll("td");
+    if (tds.length < 6) continue;
+
+    const applied = tds[6].innerText.trim();
+    if (applied !== "접수완료") continue;
+
+    const url = tds[2].querySelector("a")?.href;
+    const title = tds[2].innerText.trim();
+    const author = tds[3].innerText.trim();
+    if (!url || !title || !author) continue;
+    
+    const dateHTML = tds[4].innerHTML
+      .trim()
+      .replace(/&nbsp;/g, "")
+      .trim();
+    const [dateStr, timeRangeStr] = dateHTML
+      .split("<br>")
+      .map((str) => str.trim());
+    if (!dateStr || !timeRangeStr) continue;
+
+    events.push({ url, title, author, dateStr, timeRangeStr });
+  }
+
+  return events;
+}
+
+async function getTotalpages(baseUrl){
+  const res = await fetch(baseUrl, { credentials: "include" });
+  const html = await res.text();
+  const container = document.createElement("div")
+  container.innerHTML = html
+  const totalStr = container.querySelector(".bbs-total strong.color-blue")?.nextSibling?.textContent
+  const total = parseInt(totalStr?.replace(":", "")?.trim()) || 0;
+  const totalPages = Math.ceil(total / 10);
+  return totalPages;
+}
+
+async function getAllMentoringEvents() {
+  const events = [];
+
+  const baseUrl = "https://www.swmaestro.org/sw/mypage/userAnswer/history.do?menuNo=200047";
+  const totalPages = await getTotalpages(baseUrl)
+
+  for (let page = 1; page <= totalPages; page++) {
+    const url = new URL(baseUrl);
+    url.searchParams.set("pageIndex", page.toString());
+
+    const res = await fetch(url, { credentials: "include" });
+    const html = await res.text();
+    const pageEvents = extractEventListFromHTML(html);
+    events.push(...pageEvents);
+  }
+
+  for (let ev of events) {
+    const datePart = ev.dateStr.split("(")[0].trim(); // "2025-04-10"
+    const [startTime, endTime] = ev.timeRangeStr
+      .split("~")
+      .map((s) => normalizeTimeStr(s.trim())); // "18:30:00"
+
+    ev.startAt = new Date(`${datePart}T${startTime}`);
+    ev.endAt = new Date(`${datePart}T${endTime}`);
+    ev.timeRangeStr = `${startTime.replace(/:\d{2}$/, "")} ~ ${endTime.replace(
+      /:\d{2}$/,
+      ""
+    )}`;
+  }
+
+  events.sort((a, b) => a.startAt - b.startAt);
+  return events;
+}
+
+function getMin(timeStr){
+  let splitTime = timeStr.split(":");
+  return (parseInt(splitTime[0]) * 60) + parseInt(splitTime[1]) 
+}
+
+function convertLectureDictionary(lectures){
+  let lecturesDictionary = Object()
+  for(let i=0;i<lectures.length;i++){
+      if(!lecturesDictionary.hasOwnProperty(lectures[i].dateStr)){
+        lecturesDictionary[lectures[i].dateStr] = []
+      }
+      lecturesDictionary[lectures[i].dateStr].push(lectures[i].timeRangeStr)
+  }
+  
+  return lecturesDictionary
+}
+
+function convertLectureDictionaryWithoutDate(lectures){
+  let lecturesDictionary = Object()
+  for(let i=0;i<lectures.length;i++){
+    dateStrWithoutDate = lectures[i].dateStr.slice(0, -3)
+      if(!lecturesDictionary.hasOwnProperty(dateStrWithoutDate)){
+        lecturesDictionary[dateStrWithoutDate] = []
+      }
+      lecturesDictionary[dateStrWithoutDate].push(lectures[i].timeRangeStr)
+  }
+  
+  return lecturesDictionary
+}
